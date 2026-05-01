2306198f18a64f0343b5320f249b06b39fdf61d8
ChrisBM <chrisblanquet28@gmail.com>
2026-05-01 16:47:53 -0600

    Se subira azure, en esta copia aun no esta dockerizado

---
+++ src/main/java/com/api/api_gateway/config/GatewayCorsConfig.java
@@ -0,0 +1,33 @@
+package com.api.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class GatewayCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        
+        // La lista VIP exacta para tu frontend
+        config.setAllowedOrigins(Arrays.asList(
+            "http://localhost:5173", 
+            "http://26.*:5173"
+        ));
+        
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
